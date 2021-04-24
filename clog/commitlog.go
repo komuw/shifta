eb788b0c61bccf51ccbd0942114db754d34fac4c
komuW <komuw05@gmail.com>
2021-04-24 22:31:10 +0300

    fix tests

--- clog/commitlog.go
+++ clog/commitlog.go
@@ -299,7 +299,7 @@ func (l *Clog) Clean() error {
 	return nil
 }
 
-const maxToRead = ((1 * 1000 * 1000 * 1000) / 4) // 0.25GB
+const maxToRead = (64 * 1000 * 1000) // 64Mb
 
 // Read reads data from the commitlog starting at offset(exclusive)
 //
